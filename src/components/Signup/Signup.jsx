afc6d11633bf6689ddc77cf692d0fa6969eeb056
developerdesigner18 <developer.designer18@gmail.com>
2023-07-03 15:32:56 +0530

    Setup project

---
+++ src/components/Signup/Signup.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div>
+      Signup
+      <Button>Signup</Button>
+    </div>
+  );
+};
+
+export default Signup;
